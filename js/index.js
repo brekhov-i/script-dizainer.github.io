ba03654fd68a774fcb0b67155a020c7b4f966f0d
User <user@Users-MacBook-Air.local>
2019-04-27 10:20:03 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -3,5 +3,8 @@ $(document).ready(function () {
         //2. Получить элемент, к которому необходимо добавить маску
         $("#phone").mask("+7 (999) 999-99-99");
     });
-    
+    $('#carouselExampleIndicators').on('slid.bs.carousel', function (e) {
+        var carousel = $('#carouselExampleIndicators'), index = $('.carousel-item.active', carousel).index() + 1;
+        $('#result').val(index);
+    })
 });
