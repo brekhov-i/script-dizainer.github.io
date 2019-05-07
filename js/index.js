0b731c6c6cc52d569c839376b4f23d5d3051cc10
User <user@Users-MacBook-Air.local>
2019-05-07 16:32:19 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 $(document).ready(function () { 
-    $(function () {
         //2. Получить элемент, к которому необходимо добавить маску
-        $("#phone").mask("+7 (999) 999-99-99");
+    var form = document.getElementsByClassName('contactForm'), i=0, arr=[], current;
+    var phone = document.getElementsByName('phone'), i = 0, arr = [], current;
+    $(phone).each(function () {
+        $(phone).mask("+9 (999) 999-99-99");
+        console.log(phone.length)
     });
     $('#carouselExampleIndicators').on('slid.bs.carousel', function (e) {
         var carousel = $('#carouselExampleIndicators'), index = $('.carousel-item.active', carousel).index() + 1;
@@ -10,5 +13,18 @@ $(document).ready(function () {
     $('#carousel').on('slid.bs.carousel', function (e) {
         var carousel = $('#carousel'), index = $('.carousel-item.active', carousel).index() + 1;
         $('output[name="slidenumber"]').val(index);
+    })
+    $(document).ready(function() {
+        var margin = 100;
+        $("a").click(function() {
+            $("html, body").animate({
+                scrollTop: $($(this).attr("href")).offset().top+margin+ "px"
+            },
+            {
+                duration: 1600,
+                easing: "swing"
+            });
+            return false;
+        })
     })
 });
